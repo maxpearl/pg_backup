ca0d90a768be53a3fbafe703f38458b4356f5012
Max Pearl <maxwellbpearl@gmail.com>
2020-03-12 12:05:07 -0700

    Adding date suffix to file

--- pg_backup.py
+++ pg_backup.py
@@ -1,20 +1,26 @@
 import os
 import sh
 import click
+import datetime
 from simple_AWS.s3_functions import *
-from bu_globals import aws_profile, aws_region, local_tmp, backup_bucket
+from bu_globals import aws_profile, aws_region, backup_bucket
 
 @click.command()
 @click.option('--user', type=str, help="Postgresql Username")
 @click.option('--password', type=str, help="Postgresql Password")
 @click.option('--database', type=str, help="Postgresql Database")
+@click.option('--date_suffix', is_flag=True, help="Add a date suffix to file")
 
-def backup(user, password, database):
+def backup(user, password, database, date_suffix):
     """
     Back up PG database, copy to S3
     """
+    now = str(datetime.datetime.now())
     # backup
-    s3_file_name = f"{database}.psql"
+    if date_suffix:
+        s3_file_name = f"{database}-{now}.psql"
+    else:
+        s3_file_name = f"{database}.psql"
     db_dump = sh.pg_dump("-U", f"{user}","-w", f"{database}")
     
     # upload to S3
